bf102ca22d41b8a14a1502e99d93850052c5b000
Jules Leow <jleow@iupui.edu>
2017-06-13 19:50:24 -0400

    Create Section.CSS

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './Header.js'
 import Footer from './Footer.js'
 import Section from './Section.js'
+import ArticleLinks from './ArticleLinks.js'
 
 class App extends Component {
     render() {
     return (
       <div className="App">
         <Header />
+        <Section />
         
         <Footer />
 
@@ -23,30 +25,8 @@ class App extends Component {
               </div>
             </div>
             
-            <div className="article-links">
-              <a className="article-link" href="#">
-                <i className="fa fa-comments-o"></i>
-                <span className="article-link-text">Comments</span>
-              </a>
-              <a className="article-link" href="#">
-                <i className="fa fa-share"></i>
-                <span className="article-link-text">Share Post</span>
-              </a>
-            </div>
+            
           </div>
-          <aside className="large-4 medium-12 columns">
-            <div className="ad">
-              <h3>The Last Hammer You'll Ever Need</h3>
-              <div className="ad-image">
-                <img src="https://s-media-cache-ak0.pinimg.com/originals/b4/d5/ab/b4d5ab4a82b8f7efc1adaf548df1d089.jpg" alt="hammer" />
-                <a href="#">visit sponsor</a>
-              </div>
-              <h4>Mining diamonds or crushing the skulls of your enemies, the SmashMaster 4000 does it all</h4>
-              <p className="ad-text">Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium.</p>
-
-              <p className="ad-text">Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
-            </div>
-          </aside>
 
           <div className="small-12 columns other-articles">
             <h2>From around the Realm</h2>
@@ -76,9 +56,7 @@ class App extends Component {
               
             </div>
           </div>
-        </main>
-
-  
+        </main>  
     );
   }
   }
