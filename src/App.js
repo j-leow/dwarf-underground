9c86abeb0ac16ead64762787ca3eb4e466b0ad36
Jules Leow <jleow@iupui.edu>
2017-06-13 23:46:15 -0400

    Update App.js to include all dependencies.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,24 @@
 import React, { Component } from 'react';
 import './css/App.css';
 import Header from './Header.js'
-import Footer from './Footer.js'
 import Section from './Section.js'
-import ArticleLinks from './ArticleLinks.js'
 import Aside from './Aside.js'
 import OtherArticles from './OtherArticles.js'
+import Footer from './Footer.js'
 
 class App extends Component {
     render() {
     return (
       <div className="App">
         <Header />
-        <Section />
-        <Aside />
-        <ArticleLinks />
-        <OtherArticles />
-        
-        <Footer />
 
         <main className="expanded row">
-          
-
-                  <div className="App">
-        <Header />
-        <main className="expanded row">
-          <Article />
-          <Advertisement />
+          <Section />
+          <Aside />
           <OtherArticles />
         </main>
         <Footer />
       </div>
-            
-            
-          </div>
-
-          
-        </main>  
     );
   }
   }
